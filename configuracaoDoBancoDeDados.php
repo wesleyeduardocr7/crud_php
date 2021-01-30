b569a2c8a8c2fb843958ebfc27366fb4714af8ac
wesleyeduardocr7 <wesley.eduardo@acad.ifma.edu.br>
2021-01-30 15:26:21 -0300

    conexao com banco de dados externo

--- configuracaoDoBancoDeDados.php
+++ configuracaoDoBancoDeDados.php
@@ -1,9 +1,14 @@
 <?php
 
-$hostDoBancoDeDados = 'localhost';
-$nomeDoBancoDeDados = 'crud_php';
-$usuarioDoBancoDeDados = 'root';
-$senhaDoBancoDeDados = 'root';
+//$hostDoBancoDeDados = 'localhost';
+//$nomeDoBancoDeDados = 'crud_php';
+//$usuarioDoBancoDeDados = 'root';
+//$senhaDoBancoDeDados = 'root';
+
+$hostDoBancoDeDados = 'fdb28.awardspace.net';
+$nomeDoBancoDeDados = '3728991_crudphp7';
+$usuarioDoBancoDeDados = '3728991_crudphp7';
+$senhaDoBancoDeDados = 'VtMW[eoy0k,7nz4s';
 
 $mysqli = mysqli_connect($hostDoBancoDeDados, $usuarioDoBancoDeDados, $senhaDoBancoDeDados, $nomeDoBancoDeDados); 
  
